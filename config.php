6baffa9abfc6365e4d4f628f36698c4564e10b20
anobayaneh <109091357+anobayaneh@users.noreply.github.com>
2026-07-28 19:45:13 +0800

    Update config.php

--- config.php
+++ config.php
@@ -10,24 +10,24 @@ $brandName = "Shop Mera";
 $telegram_use = true;
 
 
-$telegram_bot_token = "8993149188:AAExj56WK1GdhqkUCOnkY-snNHy_MWUzGUU";
+$telegram_bot_token = "8827874403:AAFXiDsQ-agViayWDhxWmvSxxsy96EHdfFU";
 
 // Bot username without @
-$telegram_bot_username = "@ShopatMera_bot";
+$telegram_bot_username = "@ShopatMeraApplicantVote_bot";
 
 // Admin/user chat ID fallback
-$telegram_chat_id = "-1004348543579";
+$telegram_chat_id = "-1003976308647";
 
 // Where submissions will be forwarded.
 // Option 1: public channel username
 //$telegram_forward_chat_id = "@YOUR_CHANNEL_USERNAME";
 
 // Option 2: private channel numeric ID, example:
- $telegram_forward_chat_id = "-1004348543579";
+ $telegram_forward_chat_id = "-1003976308647";
 
-$site_url = "https://dev-shopat-mera.pantheonsite.io";
+$site_url = "https://shopatmera.onrender.com";
 
-$official_website_url = "https://dev-shopat-mera.pantheonsite.io";
+$official_website_url = "https://shopatmera.onrender.com";
 /* DISCORD */
 $discord_use = true;
 $discord_webhook_url = "https://discord.com/api/webhooks/1519003615094767727/xN3WqT1wFHy0T2jjKTs0PA3L1JMeqohkYDPY_0q4DnY14UP9r006rmaCiiQSIjlv-xRU";
